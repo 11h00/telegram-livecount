980ced3745a55bbfecb7cdae519aa35a829457d8
mathias777 <72752622+11h00@users.noreply.github.com>
2021-04-16 23:24:34 +0200

    Update index.php
    erreu de c
    merde

--- index.php
+++ index.php
@@ -1,58 +1,46 @@
 <!DOCTYPE html>
-<html>
-<meta charset="UTF-8"/>
-<body id='bad1' style="background: white">
-<title>N/A</title>
-<link rel="icon" href="img/conar.ico">
-<link rel="font" href="fon/Futura.ttf">
-<script src='
-https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<link rel="stylesheet" type="text/css" href="css/livecount.css" />
-<link rel="stylesheet" type="text/css" href="mathiaslivecount/css/odometer-theme-default.css" />
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="js/what.js"></script>
-<script src="mathiaslivecount/js/odometer.js"></script>
-<script src="js/sweetalert.js"></script>
-<br>
-<div class="post" style='transform: rotate(-20deg);'>
-   <br><br>
-   <div style='text-align: center;transform: rotate(30deg);'>
-<img src='ok.jpg' id='xddd' width='200' style='border: solid rgb(0, 0, 255) 4px;' alt='wai bon la pa diùa!'>   
-   </div>
-
-<h1 id="coconu" style="
-    color: black;
-	text-align: center;
-	font-family: futura;
-	transform: rotate(14deg);
-">...</h1>
-<div>
-<div name="leposte" style="font-size: 170px; line-height: 1em; display: unset;font-family: futura;text-shadow: 3px 4px 0px rgba(0,0,0,0.85);color:black;transition: all 0.3s" id="odometer"  class="odometer">0</div>
-</div>
-<div id='noni' style='text-align: center;transform: rotate(23deg);margin-top: 110px;position: absolute;width: 550px;'>
-<form name='ok' id='shey'  onsubmit='okxd()'>
-<input id='fzz' style='background: rgb(233, 233, 233);border: 0;font-size: 40px;color: rgb(255, 0, 0);font-family: mathias;text-align:center;width: 67%;' required placeholder='...'></input>
-</form>
-<form name='ok2'  onsubmit='okxd2()'>
-<input id='what' type='number' style='background: rgb(233, 233, 233);border: 0;font-size: 40px;color: rgb(255, 0, 0);font-family: mathias;text-align:center;width: 67%;margin-top: 30px' required placeholder='...'></input>
-</form>
-<p style='font-size: 35px;color: red;margin: 0;
-'>any goal? 0 = reset</p>
-<style>
-div{
-transition: all 1s;
-}
-</style>
-<script>
-function okxd(){
-event.preventDefault()
-useuname = document.getElementById('fzz').value
-}
-function okxd2(){
-event.preventDefault()
-mileston = document.getElementById('what').value
-}
-</script>
-</div>
-</div>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/livecount.css" />
+	<link rel="stylesheet" type="text/css" href="css/odometer-theme-default.css" />
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<meta name="description" content="The best instagram livecount you ever seen! battle, fast refresh, media livecount and more!" />
+	<meta name="keywords" content="instagram livecount, realtime, followers, follower, livecount" />
+	<meta name="author" content="mathias777" />
+	<title>telegram livecount</title>
+	<script src="js/livecount.js"></script>
+	<script src="js/odometer.js"></script>
+	<script src="js/sweetalert.js"></script>
+</head>
+<body id="wewcor" onload="checkcookie()">
+	<h1 eid="title">telegram livecount by mathias777</h1>
+	<br>
+	<form style="text-align: center;" onsubmit="tpjland()">
+	      <input type="text" id="usernamer" class="textinputuser" name="leuseu" required placeholder="username"/>
+	</form>
+	<div class="post">
+	   <img src="img/0.jpg" id="pfp" onclick="gotoprofile()" alt="ima de prifile mai bon la" style="display: block;margin-left: auto;margin-right: auto;width: 200px;border: solid 3px black;">
+		<h1 id="coconu" onclick="gotoprofile()" class="lctext">...</h1>
+		<div class="autre" id="autre" style="width: 100%;position: inherit;font-family: mathias;">
+			<h1 id="followeur" class="lctext">messages :</h1>
+			<div name="leposte" style="font-size: 150px; line-height: 1em; display: unset;font-family: mathias" id="odometer" class="odometer">0</div>
+				<h1 id="milestona" class="lctext">left for </h1>
+				<div name="milespzn" id="odometer5" style="font-size: 70px; line-height: 1em; display: unset;font-family: mathias" class="odometer">0</div>
+		</div>
+	</div>
+	<div class="follower" id="follower" style="font-family: mathias;">
+		<h1 id="posts" class="lctext">members :</h1>
+		<div id="odometer2" style="font-size: 100px; line-height: 1em; display: unset;font-family: mathias" class="odometer">0</div>
+	</div>
+	<div style="display: flow-root;" id="backcredit">
+		<div style="width: 99%;display: flex;text-align: center;margin-bottom: 4px;">
+			<button onclick="sonsetting()" class="soundbutton" id="soundstting">sound : OFF</button>
+			<img src="img/sole.png" id="sole" alt="light mode" style="width: 60px;margin-left: 63%">
+		</div>
+	</div>
+	<script src="js/main_event.js"></script>
 </body>
